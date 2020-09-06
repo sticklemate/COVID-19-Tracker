db21e0901e1d85055c272ab3b4c3f9e67ab4ea93
sticklemate <sticklemate@gmail.com>
2020-09-06 19:19:55 +1000

    Add on change event handler

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
 
+/* Shows a Line Chart for Worldwide and Bar chart for Individual countr */
+
 class App extends React.Component {
 
 //Don't need React constructor since below will immediately constructed in the React back end
     state = {
-
+        //Since App is parent component of both Cards, Charts, country picker
         data : {},
+        country:'',
     }
 
     async componentDidMount (){
@@ -17,15 +20,23 @@ class App extends React.Component {
     
     }
 
+    handleCountryChange = async (country)=>{
+        //fetch the data then set the state
+        const fetchedData = await fetchData(country);
+        
+        this.setState({ data : fetchedData, country: country});
+        
+    }
+
     render(){
 
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
             <Cards data={ data }/>
-            <CountryPicker />
-            <Chart />            
+            <CountryPicker handleCountryChange={this.handleCountryChange}/>
+            <Chart data={data} country={country}/>            
             </div>
         )
     }
