8266f1956ace036df0863ba9e5ceeced578c770a
sticklemate <sticklemate@gmail.com>
2020-09-06 19:56:50 +1000

    Updates

---
+++ precache-manifest.e47554d80c8d907e504db89fb32a8c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce280ebcefcfa290d864a3a520c4d64",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "c30c4eb98cec7f3baa12",
+    "url": "/COVID-19-Tracker/static/css/main.de3f22ae.chunk.css"
+  },
+  {
+    "revision": "ff2f4aae352288c05c63",
+    "url": "/COVID-19-Tracker/static/js/2.cd13f7d3.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-Tracker/static/js/2.cd13f7d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30c4eb98cec7f3baa12",
+    "url": "/COVID-19-Tracker/static/js/main.66bd3f58.chunk.js"
+  },
+  {
+    "revision": "ecbf061d419e4563abd7",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.1a2107af.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/covid_img.d7265326.png"
+  }
+]);
