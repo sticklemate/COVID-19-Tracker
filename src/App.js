3d0e8328bb71e052e5590b4c4bdc93d5165bd6ba
sticklemate <sticklemate@gmail.com>
2020-09-06 18:06:06 +1000

    Add states and update api changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
+import {fetchData} from './api';
 
 class App extends React.Component {
 
+//Don't need React constructor since below will immediately constructed in the React back end
+    state = {
+
+        data : {},
+    }
+
+    async componentDidMount (){
+        const fetchedData = await fetchData();
+        this.setState({ data : fetchedData});
+    
+    }
+
     render(){
+
+        const { data } = this.state;
+
         return (
             <div className={styles.container}>
-            <Cards />
-            <Chart />
+            <Cards data={ data }/>
             <CountryPicker />
+            <Chart />            
             </div>
         )
     }
