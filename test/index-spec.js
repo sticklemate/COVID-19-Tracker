5d16725c684682a0ea9c339194ce5ba735dc685a
sticklemate <sticklemate@gmail.com>
2020-10-04 15:44:19 +1100

    Add sample unit test case

---
+++ test/index-spec.js
@@ -0,0 +1,9 @@
+
+describe('Covid Tracker', function() {
+    describe('Render COVID Graph', function() {
+        it('Renders the Root App component', function () {
+            // Add test cases in the future builds
+
+        });
+    });
+});
